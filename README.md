9c0bcf1c8093b855e2ac1145cab229237f490323
Kohei YOSHIDA <kohei.yoshida@gehirn.co.jp>
2014-04-11 14:25:52 +0900

    initialize

---
+++ README.md
@@ -0,0 +1,7 @@
+# py3oidc
+
+[![Build Status](https://travis-ci.org/GehirnInc/py3oidc.svg?branch=master)](https://travis-ci.org/GehirnInc/py3oidc)
+[![Coverage Status](https://coveralls.io/repos/GehirnInc/py3oidc/badge.png?branch=master)](https://coveralls.io/r/GehirnInc/py3oidc?branch=master)
+
+## About
+This is a OpenID Connect library for Python 3 developed at [GehirnInc](http://www.gehirn.co.jp/).
---
+++ setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from setuptools import setup, find_packages
+
+requires = []
+tests_require=[
+    'nose',
+    'coverage'
+]
+
+here = os.path.dirname(__file__)
+def _read(name):
+    try:
+        return open(os.path.join(here, name)).read()
+    except:
+        return ""
+
+
+readme = _read("README.md")
+license = _read("LICENSE.md")
+
+setup(
+    name='py3oidc',
+    version='0.0.1',
+    test_suite='py3oidc',
+    author='Kohei YOSHIDA',
+    author_email='kohei.yoshida@gehirn.co.jp',
+    description='OpenID Connect library for Python 3.',
+    long_description=readme,
+    license=license,
+    url='https://github.com/GehirnInc/py3oidc',
+    packages=find_packages(),
+    install_requires=requires,
+    tests_require=tests_require,
+)
