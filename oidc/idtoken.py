b37fc7b24847a3c8741503993de06a49086f3d72
Kohei YOSHIDA <kohei.yoshida@gehirn.co.jp>
2014-04-18 18:22:26 +0900

    impl IDToken#issue

--- oidc/idtoken.py
+++ oidc/idtoken.py
@@ -13,19 +13,52 @@ def is_auth_time_required(idtoken):
     return False
 
 
-def is_nonce_required(idtoken):
-    # TODO: implement here
-    return False
+class ListOrString(list):
+
+    def serialize(self):
+        if len(self) is 1:
+            return self[0]
+        return self
+
+    @classmethod
+    def deserialize(cls, value):
+        if isinstance(value, list):
+            inst = cls()
+            inst.extend(value)
+            return inst
+        elif isinstance(value, str):
+            inst = cls()
+            inst.append(value)
+            return inst
 
 
 class IDToken(Message):
     iss = Parameter(str, required=True)
     sub = Parameter(str, required=True)
-    aud = Parameter(str, required=True)
+    aud = Parameter(ListOrString, required=True)
     exp = Parameter(int, required=True)
     iat = Parameter(int, required=True)
-    auth_time = Parameter(int, required=is_auth_time_required)
-    nonce = Parameter(str, required=is_nonce_required)
+    auth_time = Parameter(int)
+    nonce = Parameter(str)
     acr = Parameter(str)
     amr = Parameter(str)
     azp = Parameter(str)
+
+    @classmethod
+    def issue(cls, provider, owner, client):
+        iat = datetime.utcnow()
+        inst = cls.from_dict({
+            'iss': provider.get_iss(),
+            'sub': owner.get_sub(),
+            'aud': ListOrString.deserialize(client.get_id()),
+            'exp': self.unix_time(iat, provider.get_id_token_lifetime()),
+            'iat': self.unix_time(iat),
+        })
+        return inst
+
+    @staticmethod
+    def unix_time(now, delta=0):
+        if isinstance(delta, int):
+            delta = timedelta(seconds=delta)
+
+        return int(time.mktime((now + delta).timetuple()))
