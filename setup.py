c9c29c44505eb193b24a70a824f3637c697241fb
Kohei YOSHIDA <kohei.yoshida@gehirn.co.jp>
2014-06-27 17:19:15 +0900

    update py3oauth2 to 0.4.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 here = os.path.dirname(__file__)
 requires = [
-    'py3oauth2==0.4.1',
+    'py3oauth2==0.4.2',
     'jwt==0.2.3',
 ]
 tests_require = [
